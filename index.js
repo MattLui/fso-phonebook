f76b8681e7249d6c7b11163e1d7b07660ee67bda
Matt Lui <Mattclui@gmail.com>
2023-12-04 11:58:32 -0800

    3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
+app.use(morgan(':body'))
 
 let persons = [
   { 
