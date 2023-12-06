ccdd60d8f5cfeab010c763baeff0fff586fde9f9
Matt Lui <Mattclui@gmail.com>
2023-12-05 16:15:18 -0800

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(morgan(':body'))
+app.use(cors())
 
 let persons = [
   { 
